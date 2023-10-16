6e9ee12e12367b64161411b487f717458264aa3f
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2023-10-16 18:27:33 +0200

    Design page édition

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -72,3 +72,23 @@ for (let i = 0; i < buttonElement.length; i++) {
         afficherProjets(catMap)
     })  
 }
+
+//document.cookie = "admin=false; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+if (document.cookie.includes('admin=true')) {
+    
+    const logout = document.getElementById('login')
+    logout.innerText = 'logout'
+
+    //Ajout d'une margin bottom au header
+    const header = document.querySelector('header')
+    header.classList.add('header-edition')
+
+    const notDisplayed = document.querySelectorAll('.notDisplayed')
+    for (let i = 0; i < notDisplayed.length; i++) {
+        notDisplayed[i].classList.remove('notDisplayed')
+    }
+
+    const categories = document.querySelector('.button-container')
+    categories.innerText = ''    
+}
