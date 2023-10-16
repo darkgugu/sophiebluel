17953d9fb5a566a6429d0c0418b199f6112ef194
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2023-10-16 14:38:43 +0200

    Tri par catégories

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -1,30 +1,39 @@
 const projets = await fetch('http://localhost:5678/api/works').then(projets => projets.json())
-console.log('projets :', projets);
+//console.log('projets :', projets);
 
 const galleryElement = document.querySelector('.gallery')
 
-for (let i = 0; i < projets.length; i++) {
+function afficherProjets(array) {
+
+    galleryElement.innerText = ''
+
+    for (let i = 0; i < array.length; i++) {
     
-    const figureElement = document.createElement('figure')
-    const imgElement = document.createElement('img')
-    const captionElement = document.createElement('figcaption')
-
-    imgElement.setAttribute('src', projets[i].imageUrl)
-    imgElement.setAttribute('alt', projets[i].title)
-    captionElement.innerText = projets[i].title
-
-    figureElement.appendChild(imgElement)
-    figureElement.appendChild(captionElement)
-    galleryElement.appendChild(figureElement)
+        const figureElement = document.createElement('figure')
+        const imgElement = document.createElement('img')
+        const captionElement = document.createElement('figcaption')
+    
+        imgElement.setAttribute('src', array[i].imageUrl)
+        imgElement.setAttribute('alt', array[i].title)
+        captionElement.innerText = array[i].title
+    
+        figureElement.appendChild(imgElement)
+        figureElement.appendChild(captionElement)
+        galleryElement.appendChild(figureElement)
+    }
 }
 
+afficherProjets(projets)
+
+
 const categories = await fetch('http://localhost:5678/api/categories').then(categories => categories.json())
-console.log('categories :', categories);
+//console.log('categories :', categories);
 
 const buttonContainer = document.querySelector('.button-container')
 
 const defaultButton = document.createElement('button')
 defaultButton.classList.add('button-categories', 'button-categories-selected')
+defaultButton.setAttribute('id', 'button0')
 defaultButton.innerText = "Tout"
 buttonContainer.appendChild(defaultButton)
 
@@ -32,10 +41,34 @@ for (let i = 0; i < categories.length; i++) {
     
     const button = document.createElement('button')
     button.classList.add('button-categories')
+    button.setAttribute('id', 'button' + (i + 1))
 
     button.innerText = categories[i].name
     buttonContainer.appendChild(button)
 }
 
+const buttonElement  = document.querySelectorAll('.button-categories')
 
+for (let i = 0; i < buttonElement.length; i++) {
 
+    buttonElement[i].addEventListener('click', function (){
+
+        const current = document.querySelector('.button-categories-selected')
+        current.classList.remove('button-categories-selected')
+        buttonElement[i].classList.add('button-categories-selected')
+        
+        let catMap = []
+
+        if (i !== 0) {
+            catMap = projets.filter(function (x){
+                return x.categoryId === i
+            })            
+        }
+        else{
+            catMap = projets
+        }
+
+        console.log('catMap :', catMap);
+        afficherProjets(catMap)
+    })  
+}
